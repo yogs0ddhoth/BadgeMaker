d7a62fba3e965d2a9732f9ac7535f153ebf67b7e
yogs0ddhoth <blin7.webdev@gmail.com>
2022-10-29 15:30:18 -0400

    Util.WriteCSV method created

--- Program.cs
+++ Program.cs
@@ -33,28 +33,13 @@
 
         return employees;
     }
-    static void PrintEmployees(List<Employee> employees)
-    {
-        Console.WriteLine("Employees:");
 
-        int count = employees.Count;
-        for (int i = 0; i < count; i++) 
-        {
-            string template = "{0, -10}\t{1, -20}\t{2}";
-            Console.WriteLine(
-                String.Format(
-                    template, 
-                    employees[i].GetId(), 
-                    employees[i].GetFullName(), 
-                    employees[i].GetPhotoUrl())
-            );
-        }
-    }
 
     static void Main(string[] args)
     {
         List<Employee> employees = GetEmployees();
-        PrintEmployees(employees);
+        Util.PrintEmployees(employees);
+        Util.WriteCSV(employees);
     }
 
     void HelloWorld() 
--- Util.cs
+++ Util.cs
@@ -9,14 +9,38 @@ namespace CatWorx.BadgeMaker
             int count = employees.Count;
             for (int i = 0; i < count; i++) 
             {
-                string template = "{0, -10}\t{1, -20}\t{2}";
                 Console.WriteLine(
                     String.Format(
-                        template, 
+                        "{0, -10}\t{1, -20}\t{2}", 
                         employees[i].GetId(), 
                         employees[i].GetFullName(), 
                         employees[i].GetPhotoUrl())
                 );
+            }
+        }
+
+        public static void/*?*/ WriteCSV(List<Employee> employees)
+        {
+            if (!Directory.Exists("data"))         // if 'data' folder does not exist:
+            {   
+                Directory.CreateDirectory("data"); // create it.
+            }
+            // write_to_file(path: data/employees.csv)
+            using (StreamWriter file = new StreamWriter("data/employees.csv"))
+            {
+                file.WriteLine("ID,Name,PhotoUrl");
+
+                int count = employees.Count;
+                for (int i = 0; i < employees.Count; i++)
+                {
+                    file.WriteLine(
+                        String.Format(
+                            "{0},{1},{2}", 
+                            employees[i].GetId(), 
+                            employees[i].GetFullName(), 
+                            employees[i].GetPhotoUrl())
+                    );
+                }
             }
         }
     }
